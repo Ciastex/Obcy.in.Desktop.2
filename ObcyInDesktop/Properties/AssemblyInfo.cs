d0dd93b0e5bbbe375b47b309b1fac275bc5245cd
Tomasz Cichoń <mrciastexx@gmail.com>
2016-04-24 10:36:05 +0200

    2.0 release!

--- ObcyInDesktop/Properties/AssemblyInfo.cs
+++ ObcyInDesktop/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Obcy.in Desktop")]
-[assembly: AssemblyDescription("Bring some aliens to your life.")]
+[assembly: AssemblyDescription("Bring some strangers to your life.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Obcy.in Desktop")]
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.145.5910")]
-[assembly: AssemblyFileVersion("1.0.145.5910")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
